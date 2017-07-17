50aa4e3acbb4759dc83c4699c8076da61c1af47d
qiuqiuyuan <1511732855@qq.com>
2017-07-17 14:34:04 +0800

    add

---
+++ Environment/environment.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2016/3/30
+ * Time: 17:34
+ * 环境说明：
+ * PRODUCTION = 生产环境 production
+ * STAGING （这个不清楚）
+ * TESTING = 测试环境（75服务器用） testing
+ * DEVELOPMENT = 开发环境  development
+ */
+
+// Common environment type constants for consistency and convenience
+//const PRODUCTION  = 10;
+//const STAGING     = 20;
+//const TESTING     = 30;
+//const DEVELOPMENT = 40;
+//设置环境
+define('ENVIRONMENR','PRODUCTION');
